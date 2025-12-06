24c945f045822e7494cff9fe703039ac589d15e0
havenf <havenfricke@u.boisestate.edu>
2025-12-05 18:46:27 -0700

    model updates

--- Server/Models/Data.js
+++ Server/Models/Data.js
@@ -1,9 +1,12 @@
 class Data {
-  constructor(id, createTime, deviceId, dataValue) {
+  constructor(id, create_time, device_id, distance_cm, pitch_deg, roll_deg, yaw_deg) {
     this.id = id;
-    this.createTime = createTime;
-    this.deviceId = deviceId;
-    this.dataValue = dataValue;
+    this.createTime = create_time;
+    this.deviceId = device_id;
+    this.distanceCm = distance_cm;
+    this.pitchDeg = pitch_deg;
+    this.rollDeg = roll_deg;
+    this.yawDeg = yaw_deg;
   }
 }
 
