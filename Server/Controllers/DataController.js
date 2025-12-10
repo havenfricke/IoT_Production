8a357df8bfc880d8334d07d56d20abc9c2de2024
havenf <havenfricke@u.boisestate.edu>
2025-12-09 18:10:14 -0700

    route fix #2

--- Server/Controllers/DataController.js
+++ Server/Controllers/DataController.js
@@ -3,13 +3,13 @@ const dataService = require('../Services/DataService');
 
 class DataController extends BaseController {
   constructor() {
-    super('/data/all'); // ensure app uses app.use('/api', new DataController().router)
+    super('/data'); // ensure app uses app.use('/api', new DataController().router)
     this.router
       .get('', this.getAllData)
-      .get('/:id', this.getDataById)
-      .post('', this.createData)
-      .put('/:id', this.editData)
-      .delete('/:id', this.deleteData);
+      // .get('/:id', this.getDataById)
+      // .post('', this.createData)
+      // .put('/:id', this.editData)
+      // .delete('/:id', this.deleteData);
   }
 
   async getAllData(req, res, next) {
@@ -17,25 +17,25 @@ class DataController extends BaseController {
     catch (err) { next(err); }
   }
 
-  async getDataById(req, res, next) {
-    try { res.json({ data: await dataService.getDataById(req.params.id) }); }
-    catch (err) { next(err); }
-  }
+  // async getDataById(req, res, next) {
+  //   try { res.json({ data: await dataService.getDataById(req.params.id) }); }
+  //   catch (err) { next(err); }
+  // }
 
-  async createData(req, res, next) {
-    try { res.status(201).json({ data: await dataService.createData(req.body) }); }
-    catch (err) { next(err); }
-  }
+  // async createData(req, res, next) {
+  //   try { res.status(201).json({ data: await dataService.createData(req.body) }); }
+  //   catch (err) { next(err); }
+  // }
 
-  async editData(req, res, next) {
-    try { res.json({ data: await dataService.editData(req.params.id, req.body) }); }
-    catch (err) { next(err); }
-  }
+  // async editData(req, res, next) {
+  //   try { res.json({ data: await dataService.editData(req.params.id, req.body) }); }
+  //   catch (err) { next(err); }
+  // }
 
-  async deleteData(req, res, next) {
-    try { res.json({ result: await dataService.deleteData(req.params.id) }); }
-    catch (err) { next(err); }
-  }
+  // async deleteData(req, res, next) {
+  //   try { res.json({ result: await dataService.deleteData(req.params.id) }); }
+  //   catch (err) { next(err); }
+  // }
 }
 
 module.exports = DataController;
